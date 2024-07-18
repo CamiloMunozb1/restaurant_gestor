c89fb1b91254622ab40537938aec3e0b7dea3c3a
Camilo <camilomunozb203@gmail.com>
2024-07-17 22:04:37 -0500

    Nueva funcionalidad, incompleta aun.

--- restaurant/funciones/reservas.py
+++ restaurant/funciones/reservas.py
@@ -1,1 +1,8 @@
 import sqlite3
+
+def gestor_reservas():
+    with sqlite3.connect("C:/Users/POWER/gestor_restaurante.db") as reservas_gestor:
+        consulta_cursor = reservas_gestor.cursor()
+        fecha_reserva = str(input("Ingrese una fehca de reserva (DD/MM/YY): "))
+        hora_reserva = str(input("Ingresa la hora de la reserva: "))
+        
